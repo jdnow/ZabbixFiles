45efd3f71642d7d6db936abebbc572a046ccbc64
John Moore <jdm8976@gmail.com>
2013-04-19 20:44:14 -0400

    Hyper-V Template for Zabbix
    Template for Hyper-V. XML file is the template. The powershell script
    pulls the data from the hyper v server.

---
+++ Scripts/HyperV_Status.ps1
@@ -0,0 +1,41 @@
+﻿param(
+	[Parameter(Mandatory=$False)]
+	[string]$QueryName,
+    [string]$VMName
+)
+
+if ($QueryName -eq '') {
+    
+    $colItems = Get-VM
+
+    write-host "{"
+    write-host " `"data`":["
+    write-host
+
+    foreach ($objItem in $colItems) {
+        $line =  " { `"{#VMNAME}`":`"" + $objItem.Name + "`" , `"{#VMSTATE}`":`"" + $objItem.State + "`" },"
+        write-host $line
+    }
+
+    write-host
+    write-host " ]"
+    write-host "}"
+    write-host
+}
+
+else {
+
+    switch ($QueryName)
+        {
+        ('State') {$Results = Get-VM -Name "$VMName"  | select State | Format-Table -HideTableHeaders -AutoSize}
+        ('CPUUsage') {$Results = Get-VM -Name "$VMName" | select CPUUsage | Format-Table -HideTableHeaders -AutoSize}
+        ('MemoryAssigned') {$Results = Get-VM -Name "$VMName" | select MemoryAssigned | Format-Table -HideTableHeaders -AutoSize}
+        ('Uptime') {$Results = Get-VM -Name "$VMName" | select Uptime | Format-Table -HideTableHeaders -AutoSize}
+        ('Status') {$Results = Get-VM -Name "$VMName" | select Status | Format-Table -HideTableHeaders -AutoSize}
+        default {$Results = "Incorrect Command Given"}
+        }
+    $Results = $Results | Out-String
+    $Results = $Results.trim()
+    Write-Host $Results
+
+}
