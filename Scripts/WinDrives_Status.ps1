9b2b468fbb9d74167caa68d94353e74d582e474d
jdnow <jdm8976@gmail.com>
2013-08-12 15:02:20 -0400

    Script for Windows Volumes
    Zabbix script to grab windows disk volumes

---
+++ Scripts/WinDrives_Status.ps1
@@ -0,0 +1,40 @@
+﻿param(
+	[Parameter(Mandatory=$False)]
+	[string]$QueryName,
+    [string]$FSName
+)
+
+if ($QueryName -eq '') {
+    
+    $colItems = gwmi win32_volume | select Name,FileSystem
+
+    write-host "{"
+    write-host " `"data`":["
+    write-host
+
+    foreach ($objItem in $colItems) {
+        $objItem.Name = $objItem.Name -replace "\\","/"
+        $line =  " { `"{#FSNAME}`":`"" + $objItem.Name + "`" , `"{#FSTYPE}`":`"" + $objItem.FileSystem + "`" },"
+        write-host $line
+    }
+
+    write-host
+    write-host " ]"
+    write-host "}"
+    write-host
+}
+
+else {
+    $FSName = [regex]::escape($FSName)
+    $FSName = $FSName -replace "/","\\"
+    switch ($QueryName)
+        {
+        ('Capacity') {$Results = gwmi win32_volume -Filter "name = '$FSName'" | select Capacity | Format-Table -HideTableHeaders -AutoSize}
+        ('FreeSpace') {$Results = gwmi win32_volume -Filter "name = '$FSName'" | select FreeSpace | Format-Table -HideTableHeaders -AutoSize}
+        default {$Results = "Incorrect Command Given"}
+        }
+    $Results = $Results | Out-String
+    $Results = $Results.trim()
+    Write-Host $Results
+
+}
